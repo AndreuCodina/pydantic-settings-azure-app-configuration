02eecc9e1846d50735f091515596baee7ee66d1b
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2024-10-14 21:08:57 +0200

    Update CI

---
+++ linter_and_formatter.py
@@ -0,0 +1,13 @@
+# Linter:
+# uv run ruff check
+
+# Formatter:
+# uv run ruff format --diff
+# uv run ruff format
+
+def print_sum(a: int, b:int) -> None:
+    result= a + b
+    print(f"The result is result {resul}")
+
+
+sum(1, "5")
